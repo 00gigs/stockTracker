887ad0d759cd08f9e51de7f7dfcc133f4d805833
00gigs <124551005+00gigs@users.noreply.github.com>
2023-11-23 15:21:08 -0500

    boostrap added

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ const express  = require('express');
 const { engine } = require('express-handlebars')
 const path = require('path');
 const app = express();
+const sample = [1, 2, 3, 4, 5]
+
+
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.get('/', function (req, res) {
+    res.render('home',{sample: sample});
 });
+
 
 
 app.use(express.static(path.join(__dirname ,'./public')));
